656d193db32a59ea4460a89e508da00867f51843
yongyut-wil <yongyut.wilachan@gmail.com>
2021-07-25 21:41:06 +0700

    code to Lecture50

---
+++ Lecture50_yongyut_w.py
@@ -0,0 +1,13 @@
+def plusNumber(x,y):
+    print(x+y)
+def minusNumber(x,y):
+    print(x-y)
+def multiplyNumber(x,y):
+    print(x*y)
+def divideNumber(x,y):
+    print(x/y)
+
+plusNumber(50,3)
+minusNumber(50,3)
+multiplyNumber(50,3)
+divideNumber(50,3)
