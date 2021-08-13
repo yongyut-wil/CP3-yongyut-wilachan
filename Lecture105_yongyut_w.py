d21fd3688bfce9ceb71ee5636159e31b8f98fa91
yongyut-wil <yongyut.wilachan@gmail.com>
2021-08-13 15:41:06 +0700

    code to lecture105

---
+++ Lecture105_yongyut_w.py
@@ -0,0 +1,26 @@
+class Vehicle:
+    lecenseCode = ""
+    serialCode = ""
+    def turnOnAirConditioner(self):
+        print("Turn On : Air")
+
+class Car(Vehicle):
+    pass
+class Picup(Vehicle):
+    pass
+class Van(Vehicle):
+    pass
+class Estatecar(Vehicle):
+    pass
+
+car1 = Car()
+car1.turnOnAirConditioner()
+
+picup1 = Picup()
+picup1.turnOnAirConditioner()
+
+van1 = Van()
+van1.turnOnAirConditioner()
+
+estatecar1 = Estatecar()
+estatecar1.turnOnAirConditioner()
